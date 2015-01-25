a82e7508be52b3f41b01135a2640d44d0601b670
David Swafford <daswafford@gmail.com>
2015-01-25 09:17:28 -0800

    Py template for vim

---
+++ .vim/tmpl/tmpl.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Copyright 2015-present David Swafford.  All Rights Reserved.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+__author__ == 'David Swafford <daswafford@gmail.com>'
